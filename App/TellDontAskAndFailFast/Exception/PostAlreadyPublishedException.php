be53191429c4937d048e09b2906cc8eef3362a5c
arturcesar <artur.cesar@redevistorias.com.br>
2021-09-10 02:54:00 -0300

    Igoring files

--- App/TellDontAskAndFailFast/Exception/PostAlreadyPublishedException.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\TellDontAskAndFailFast\Exception;
-
-class PostAlreadyPublishedException extends \RuntimeException
-{
-	
-}
--- App/TellDontAskAndFailFast/Post/Status.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\TellDontAskAndFailFast\Post;
-
-class Status
-{
-	const DRAFT = 'DRAFT';
-	const PUBLISHED = 'PUBLISHED';
-}
